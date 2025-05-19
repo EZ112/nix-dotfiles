9dbcf08ff2cab1cccb331b55a12fbbd044b17d69
Izzi <izzidz@gmail.com>
2025-05-19 22:51:26 +0700

    add brightnessctl

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -58,6 +58,7 @@ in
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+     brightnessctl
      neovim 
      wget
      neofetch
